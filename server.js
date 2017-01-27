10cede2061a1e5b8ea3be957957054b589da4636
David Terry <dgterry@us.ibm.com>
2017-01-27 16:24:56 -0500

    space now

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var middleware = {
     // runs before body of route it is protecting
     // next tells express to go ahead and run the routes, otherwise error and stop
     requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
+        console.log('private route hit! ');
         next();
     },
     logger: function (req, res, next) {
